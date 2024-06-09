8be9a06dc73a71fd5f44edabfe78934952c69be9
Adnan <adbak.dev@gmail.com>
2024-06-09 21:12:05 +0200

    openai

--- utils/action.js
+++ utils/action.js
@@ -1,6 +1,22 @@
 "use server";
 
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 export const generateChatResponse = async (chatMessage) => {
-  console.log(chatMessage);
-  return "awesome";
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [{ role: "system", content: "You are a helpful assistant." }, ...chatMessage],
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+    });
+
+    return response.choices[0].message;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
